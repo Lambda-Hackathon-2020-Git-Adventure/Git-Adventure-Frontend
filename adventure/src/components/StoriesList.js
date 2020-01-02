49eadd257d94a0caf7701465fcfe8f7929910308
Krishan Nattar <krishan.nattar@gmail.com>
2020-01-02 15:45:07 -0500

    basic mapping created for stories

--- adventure/src/components/StoriesList.js
+++ adventure/src/components/StoriesList.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const StoriesList = (props) => {
-    return ( 
-        <div>STORIES LIST</div>
-     );
-}
- 
+const StoriesList = props => {
+	const [list, setList] = useState();
+	useEffect(() => {
+		axios
+			.get('www.url.com')
+			.then(res => {
+				console.log(res);
+				setList(res.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<>
+			<div>STORIES LIST</div>
+			{list &&
+				list.map(story => {
+					return (
+						<>
+							<Link to={`/story/${story.id}`}>{story.title}</Link>
+						</>
+					);
+				})}
+		</>
+	);
+};
+
 export default StoriesList;
