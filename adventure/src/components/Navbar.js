16c8db4ca440d7dab1987cf05f6f891d9d34a463
Krishan Nattar <krishan.nattar@gmail.com>
2020-01-02 15:02:00 -0500

    navbar started

--- adventure/src/components/Navbar.js
+++ adventure/src/components/Navbar.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
 
 export default function NavBar() {
-  return <div>NavBar</div>
+	return (
+		<div className="navbar-main">
+            <NavLink to ="/" className="navbar-title">Adventure</NavLink>
+			{/* <p className="navbar-title">Adventure</p> */}
+			<div className="navbar-sign-in-up">
+                <NavLink to="/">View Stories</NavLink>
+                <NavLink to="/">Sign In</NavLink>
+                <NavLink to="/">Sign Up</NavLink>
+                <NavLink to="/">Dashboard</NavLink>
+				{/* <p>View Stories</p>
+                <p>Sign in</p>
+				<p>Sign up</p>
+                <p>Dashboard</p> */}
+			</div>
+		</div>
+	);
 }
