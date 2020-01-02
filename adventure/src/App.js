af445aa398d696d43fff41bd20d606aa6be63ac3
Jonathan Ho <johnnyho82@gmail.com>
2020-01-02 14:41:59 -0500

    Work on TeammemberInfo

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,33 +14,19 @@ import StoryTree from './components/StoryTree';
 import ViewDecision from './components/ViewDecision';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Adventure Time!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Navbar />
-      <Route path='/about' component={About} />
-      <Route path='/dashboard' component={Dashboard} />
-      <Route path='/landing' component={Landing} />
-      <Route path='/modifydecision' component={ModifyDecision} />
-      <Route path='/signin' component={SignIn} />
-      <Route path='/signup' component={SignUp} />
-      <Route path='/storytree' component={StoryTree} />
-      <Route path='/viewdecision' component={ViewDecision} />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<Route path='/about' component={About} />
+			<Route path='/dashboard' component={Dashboard} />
+			<Route path='/landing' component={Landing} />
+			<Route path='/modifydecision' component={ModifyDecision} />
+			<Route path='/signin' component={SignIn} />
+			<Route path='/signup' component={SignUp} />
+			<Route path='/storytree' component={StoryTree} />
+			<Route path='/viewdecision' component={ViewDecision} />
+		</div>
+	);
 }
 
 export default App;
---
+++ adventure/src/components/TeammemberCard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TeammemberCard() {
+	return <div>TEAM MEMBER CARD</div>;
+}
