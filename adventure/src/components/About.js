5e09e94b4f0845ce3aa704b7d521ed99877142da
Jonathan Ho <johnnyho82@gmail.com>
2020-01-02 17:01:09 -0500

    Adjust About page heading

--- adventure/src/components/About.js
+++ adventure/src/components/About.js
@@ -8,7 +8,7 @@ import { team_member_info } from './TeamMemberInfo';
 export default function About() {
 	return (
 		<div>
-			<h2>Meet the team behind Git Your Own Adventure!</h2>
+			<StyledHeader>Meet the team behind Git Your Own Adventure!</StyledHeader>
 			<StyledWrapper>
 				{team_member_info.map(team_member => (
 					<TeamMemberCard key={team_member.name} team_member={team_member} />
@@ -17,6 +17,12 @@ export default function About() {
 		</div>
 	);
 }
+
+const StyledHeader = styled.h2`
+	font-size: 3.6rem;
+	width: 96%;
+	margin: 2rem auto 3rem;
+`;
 
 const StyledWrapper = styled.div`
 	width: 96%;
