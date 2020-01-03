131a7075d64726a5cbb9d9e7cf2b5492fa6ff2b9
Jonathan Ho <johnnyho82@gmail.com>
2020-01-03 16:35:47 -0500

    Fix CTA button

--- adventure/src/components/Landing.js
+++ adventure/src/components/Landing.js
@@ -15,29 +15,55 @@ const Landing = () => {
 		<div className='landing-page-container'>
 			<h1>
 				<Black>Welcome to</Black> CREATE YOUR OWN ADVENTURE<Black>...</Black>
-      </h1>
-     <div className='landing-page-top'>
-       <img className='landing-page-image' src ={photo === 1 ? './photos/cyoa1.jpg' : photo === 2 ? './photos/cyoa2.jpg' : './photos/cyoa3.jpg'}/>
-       <div className='landing-page-top-text'>
-         <div>
-        <h2>Create custom stories and share <br />
-        them with friends</h2>
-        <p>Create Your Own Adventure allows you to create your own interactive stories and make choices to explore your worlds. Collaborate with friends to create unique and compelling stories full of daring deeds and treacherous encounters. Build rich plots to explore. Become a hero in your own story.</p>
-        {/* <Link to='/signup'><img classname='cta-button' src='./photos/button.png' /><span>Get Started</span></Link>  */}
-        <div className='cta-button'>
-        <Link to='/signup'>{ctabutton()}  </Link> 
-        <Link to='/signup'><div className='cta-text'>Get<br/> Started</div></Link> 
-        </div>
-      </div>
-      </div>
-     </div>
-     <hr className='landing-page-hr' />
-    <About />
-  </div>
-    )
-}
+			</h1>
+			<div className='landing-page-top'>
+				<img
+					className='landing-page-image'
+					src={
+						photo === 1
+							? './photos/cyoa1.jpg'
+							: photo === 2
+							? './photos/cyoa2.jpg'
+							: './photos/cyoa3.jpg'
+					}
+				/>
+				<div className='landing-page-top-text'>
+					<div>
+						<h2>
+							Create custom stories and share <br />
+							them with friends
+						</h2>
+						<p>
+							Create Your Own Adventure allows you to create your own
+							interactive stories and make choices to explore your worlds.
+							Collaborate with friends to create unique and compelling stories
+							full of daring deeds and treacherous encounters. Build rich plots
+							to explore. Become a hero in your own story.
+						</p>
+						{/* <Link to='/signup'><img classname='cta-button' src='./photos/button.png' /><span>Get Started</span></Link>  */}
+						<div className='cta-button'>
+							<Link
+								to={localStorage.getItem('token') ? '/dashboard' : '/signup'}>
+								{ctabutton()}{' '}
+							</Link>
+							<Link
+								to={localStorage.getItem('token') ? '/dashboard' : '/signup'}>
+								<div className='cta-text'>
+									Get
+									<br /> Started
+								</div>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr className='landing-page-hr' />
+			<About />
+		</div>
+	);
+};
 
-export default Landing
+export default Landing;
 const Black = styled.span`
 	color: black;
 `;
