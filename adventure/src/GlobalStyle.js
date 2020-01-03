17cd741ae3246a27ef76c508198c90f5b86ae954
Jonathan Ho <johnnyho82@gmail.com>
2020-01-03 13:39:32 -0500

    Commit before pull

--- adventure/src/GlobalStyle.js
+++ adventure/src/GlobalStyle.js
@@ -28,5 +28,10 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: font77490;
+    background-color: whitesmoke;
+  }
+
+  p {
+    font-family: 'Libre Baskerville';
   }
 `;
