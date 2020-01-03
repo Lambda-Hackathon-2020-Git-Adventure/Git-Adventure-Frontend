df53329caf7118cc9e664c7d93d9e7dd2ae28106
Faye Skeen <fskeen@gmail.com>
2020-01-02 16:48:32 -0800

    added open sans to font import for p tags

--- adventure/src/GlobalStyle.js
+++ adventure/src/GlobalStyle.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://www.fontify.me/wf/7ae0c162d7a54b07d9af0bfbdba6482e');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
 
   * {
     margin: 0;
@@ -20,5 +21,6 @@ export const GlobalStyle = createGlobalStyle`
 
   p {
     max-width: 65ch;
+    font-family: "Open Sans"
   }
 `;
